d8883bcb2eaf22000087fc23364e8c59f08060c3
araufi <70168004+araufi@users.noreply.github.com>
2020-10-24 17:27:40 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -1,33 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useState} from "react";
-
-
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { useCombobox } from "downshift";
+import { input } from "antd";
 
 function App() {
+  const [inputItems, setInputItems] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [singleUser, setSingleUser] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const {
+    isOpen,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+  } = useCombobox({
+    items: inputItems,
+    onInputValueChange: ({ inputValue }) => {
+      setInputItems(
+        users.filter((item) =>
+          item.name.toLowerCase().startsWith(inputValue.toLowerCase())
+        )
+      );
+    },
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-           See a mural we didn't see? Submit below.
-        </p>
-        <form>
-          <label for="picture">Submit picture here</label>
-          <br></br>
-          <input type="text" id="pic" name="pic"></input>
-          <br></br>
-          <label for="picture">Any other information?</label>
-          <br></br>
-          <textarea id="info" rows="15" cols="40" placeholder="Artist name, Location, description, etc."></textarea> 
-          <br></br>
-          <input type="button" value="Submit" id="btnSubmit" onClick="sucessThx()"></input>
-        </form>
-
-
-
-
-      </header>
-      
+      <h2>Face The Art {singleUser}</h2>
+      <button>Use Current Location</button>
+      <div {...getComboboxProps()}>
+        <input
+          {...getInputProps()}
+          placeholder="Search"
+          enterButton="Search"
+          size="large"
+        />
+      </div>
+      <ul {...getMenuProps()}>
+        {isOpen &&
+          inputItems.map((item, index) => (
+            <span
+              key={item.id}
+              {...getItemProps({ item, index })}
+              onClick={() => setSingleUser(item.name)}
+            >
+              <li
+                style={highlightedIndex === index ? { background: "#ede" } : {}}
+              >
+                <h4>{item.name}</h4>
+              </li>
+            </span>
+          ))}
+      </ul>
     </div>
   );
 }
